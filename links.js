578dab37881ccae01c8199b0488473096afbaed9
ND Labs <nhatdang10.nd@gmail.com>
2026-09-21 22:02:22 +0700

    Update links.js

--- links.js
+++ links.js
@@ -110,5 +110,20 @@ const ndLinks = [
         iconTextColor: "#FFFFFF",
 
         newTab: true
+    },
+
+    {
+        title: "EduND - Làm bài kiểm tra (V1)",
+        description: "Trang làm bài kiểm tra bằng cách tải lên file đề.",
+        url: "/ndlabs/edu/v1",
+        image: "https://ndsite.web.app/assets/images/logo.png",
+        
+        color: "#EEF7ED",
+        textColor: "#0075F1",
+        mutedColor: "#668096",
+        iconColor: "#0075F1",
+        iconTextColor: "#FFFFFF",
+        
+        newTab: true
     }
 ];
